710398e64a3ffa3946a49f9dc48410be4c9cd309
schremsn <josef@dieselpark.com>
2018-01-30 15:13:42 +0700

    added pipeline
    added method to retrieve pipeline information to lead

--- kgk_addon/mobile/controllers/controllers.py
+++ kgk_addon/mobile/controllers/controllers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class Mobile(http.Controller):
+class Mobile(http.Controller):
 #     @http.route('/mobile/mobile/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
@@ -18,3 +18,11 @@ from odoo import http
 #         return http.request.render('mobile.object', {
 #             'object': obj
 #         })
+
+     @http.route('/mobile/pipeline_count', type='http', auth='user', methods=['GET'])
+     def mobile_pipeline_count(self, res_id, token):
+            try:
+                record.pipeline_count()
+            except Exception:
+                return MailController._redirect_to_messaging()
+            return redirect
--- kgk_addon/mobile/models/mobile.py
+++ kgk_addon/mobile/models/mobile.py
@@ -2,7 +2,22 @@
 
 from odoo import models, fields, api
 
-class customer(models.Model):
-    _name = "mobile.customer"
-    _inherits = "res.partner, crm.lead"
+class lead(models.Model):
+    _name = "crm.lead"
+    _inherit = "crm.lead"
 
+
+    @api.model
+    def pipeline_count(self):
+        #self.env.cr.execute('SELECT stage_id, count(stage_id) from crm_lead where active=true group by stage_id')
+        #return self.env.cr.fetchall()
+        result = dict();
+        crmstage = self.pool['crm.stage']
+        stages = self.env['crm.stage'].search([])
+        for stage in stages:
+            id = stage.id
+            domain = [('stage_id', '=', id), ('active', '=', 't')]
+            count = self.env['crm.lead'].search_count(domain)
+            result[id] = count;
+
+        return result
